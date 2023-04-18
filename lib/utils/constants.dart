3f76ddac35e251db300910b8868dcd98f54ec919
kradit <99046834+kradit@users.noreply.github.com>
2023-04-18 13:36:59 +0530

    updating users

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,2 +1,2 @@
-const MONGO_CONN_URL = "<MONGO_URI>";
+const MONGO_CONN_URL = "mongodb+srv://test:test@cluster0.vujat1d.mongodb.net/test?retryWrites=true&w=majority";
 const USER_COLLECTION = "users";
